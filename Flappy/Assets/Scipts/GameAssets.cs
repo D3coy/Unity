60890bf9724ffff0168862dc048f6b4e6632addb
vimtum@tutanota.com <vimtum@tutanota.com>
2021-03-11 03:22:04 +0300

    Preparation stage of project:
    - File hierarchy
    - Main textures mapped

---
+++ Flappy/Assets/Scipts/GameAssets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets instance;
+
+    // function to receive instance variable
+    public static GameAssets GetInstance()
+    {
+        return instance;
+    }
+
+    //function activating current class
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // field displayed in GameAssets game object structure
+    // also uses in GameHandler main game class as [GameAssets -> GetInstance() -> pipeHeadSprite]
+    public Sprite pipeHeadSprite;
+}
